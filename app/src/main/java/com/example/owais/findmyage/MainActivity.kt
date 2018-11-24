a5056a2a421a5385cc58108e288ab775d0b77e70
Owais Alam <owaisstar@gmail.com>
2018-11-24 12:26:02 +0500

    using click handler instead of click listner

--- app/src/main/java/com/example/owais/findmyage/MainActivity.kt
+++ app/src/main/java/com/example/owais/findmyage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.owais.findmyage
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
 import java.util.*
@@ -12,14 +13,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttonFindMyAge.setOnClickListener {
-            //input
-            var yearOfBirth:Int = editTextFindMyAge.text.toString().toInt()
-            //process
-            var currentYear = Calendar.getInstance().get(Calendar.YEAR)
-            var age = currentYear - yearOfBirth
-            //output
-            textViewFindMyAge.text = "your age is $age";
-        }
+    }
+
+    fun findAgeClicked(view:View){
+        //input
+        var yearOfBirth:Int = editTextFindMyAge.text.toString().toInt()
+        //process
+        var currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        var age = currentYear - yearOfBirth
+        //output
+        textViewFindMyAge.text = "your age is $age";
     }
 }
